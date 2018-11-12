a86c31e8322d542732f1582821443a9025bee4ff
newpeople0595 <az198071123@gmail.com>
2018-11-12 14:20:20 +0800

    [ADD] Publish GitPage

---
+++ src/settings.js
@@ -0,0 +1,38 @@
+window._CCSettings = {
+    platform: "web-desktop",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {}
+    },
+    launchScene: "db://assets/scene/select.fire",
+    scenes: [
+        {
+            url: "db://assets/scene/select.fire",
+            uuid: "94j/JysU1BR6WiTpbTxPkA"
+        }
+    ],
+    packedAssets: {
+        "092f80ea6": [
+            "02delMVqdBD70a/HSD99FK",
+            "4bq2fLGOZAmbhANV8Ec/iQ",
+            "d8HsitJHxOYqo801xBk8ev"
+        ],
+        "0b622169c": [
+            "94j/JysU1BR6WiTpbTxPkA",
+            "9bvaMerUlDyary99mJa6xp",
+            "a2MjXRFdtLlYQ5ouAFv/+R",
+            "c9+lH/PwFGAY+AMl0bEdq3"
+        ]
+    },
+    md5AssetsMap: {},
+    orientation: "",
+    debug: true,
+    subpackages: {}
+};
